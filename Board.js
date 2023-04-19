9ffd169b54b846393490c68c6bf202d815f0350d
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-04-19 13:19:54 -0400

    Create Board.js

---
+++ Board.js
@@ -0,0 +1,31 @@
+class Board {
+    constructor() {
+        this.checkBool = true;
+        this.board = [
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0]
+        ]
+        this.cellWidth = 50
+    }
+    
+    draw() {
+        for(let i = 0; i < this.board.length; i++) {
+            for(let j = 0; j < this.board[0].length; j++){
+                if(this.checkBool){
+                    fill('white')
+                } else {
+                    fill('black')
+                }
+                rect(i*cellWidth,j*this.cellWidth, this.cellWidth,this.cellWidth) 
+                this.checkBool = !this.checkBool
+                }
+                this.checkBool = !this.checkBool
+        }
+    }
+}
