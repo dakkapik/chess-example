12287fcd79ee540a44322c103da8c19cb8af78e6
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-05-01 10:51:17 -0400

    update rook for movemnent

--- Rook.js
+++ Rook.js
@@ -5,37 +5,57 @@ class Rook extends Board {
     this.y = y;
     this.active = false;
     
-    document.addEventListener("click", ()=> {
-      if(
-        mouseX > this.x *this.cellWidth&&
-        mouseX < this.x *this.cellWidth+ this.cellWidth  &&
-        mouseY > this.y *this.cellWidth&&
-        mouseY < this.y *this.cellWidth+ this.cellWidth
-      ){
-         this.active = !this.active;
-        }
+    document.addEventListener('click',()=> {
+      let x = floor(mouseX/this.cellWidth);
+      let y = floor(mouseY/this.cellWidth);
+      
+      if(this.x === x && this.y === y)
+      { this.active = !this.active }
+      
+      if(this.active && this.board[y][x] === 1){
+        this.attack(false)
+        this.x = x;
+        this.y = y;
+        this.active = false;
+      }
+      
     })
+    
   }
   
   draw(){
     fill("yellow")
-    rect(this.x*this.cellWidth,this.y*this.cellWidth, this.cellWidth, this.cellWidth)
+    
+    rect(
+      this.x * this.cellWidth, 
+      this.y * this.cellWidth, 
+      this.cellWidth, 
+      this.cellWidth
+    )
+    
     if(this.active) {
-      this.drawAttack()
+      this.attack(true)
+    } else {
+      this.attack(false)
     }
   }
   
-  drawAttack(){
-    fill("#FF000068")
+  attack(active){
     for(let y = 0; y < this.board.length; y++){
       for(let x = 0; x < this.board[0].length; x++){
         if(x === this.x || y === this.y){
-          rect(
-            x*this.cellWidth, 
-            y*this.cellWidth, 
-            this.cellWidth, 
-            this.cellWidth
-          )
+          if(active){
+            this.board[y][x] = 1;
+            fill("#FF000068")
+            rect(
+              x*this.cellWidth, 
+              y*this.cellWidth, 
+              this.cellWidth, 
+              this.cellWidth
+            )
+          } else {
+            this.board[y][x] = 0;
+          }
         }
       }
     }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 let board = new Board()
-let rook = new Rook(3,4)
+let rook1 = new Rook(0,0)
+let rook2 = new Rook(7,0)
 
 function setup() {
   createCanvas(400, 400);
+  frameRate(5);
 }
 
 function draw() {
   board.draw();
-  rook.drawAttack()
+  rook1.draw()
+  rook2.draw()
 }
